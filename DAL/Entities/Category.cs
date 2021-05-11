47a17d5457a9a610b75d2cf505674653c4f6354e
Ivon-cpp <65172913+Ivon-cpp@users.noreply.github.com>
2021-05-11 18:17:23 +0300

    Добавлены классы сущностей в DAL

--- DAL/Entities/Category.cs
+++ DAL/Entities/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace DAL.Entities
     public class Category
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
 
         public virtual List<Person> Persons { get; set; }
